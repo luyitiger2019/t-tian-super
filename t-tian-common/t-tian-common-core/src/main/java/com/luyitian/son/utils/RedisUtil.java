16a7ddcb2aa02a93a954e0330bb38cb7e56d1611
陸毅 <413852131@qqcom>
2019-08-29 13:55:51 +0800

    为唯一登录增加事务

--- t-tian-common/t-tian-common-core/src/main/java/com/luyitian/son/utils/RedisUtil.java
+++ t-tian-common/t-tian-common-core/src/main/java/com/luyitian/son/utils/RedisUtil.java
@@ -1,18 +1,15 @@
 package com.luyitian.son.utils;
 
 import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisUtil {
-	private final StringRedisTemplate stringRedisTemplate;
-
 	@Autowired
-	public RedisUtil(StringRedisTemplate stringRedisTemplate) {
-		this.stringRedisTemplate = stringRedisTemplate;
-	}
+	private StringRedisTemplate stringRedisTemplate;
 
 	/**
 	 * 存放string类型
@@ -25,10 +22,47 @@ public class RedisUtil {
 	 *            超时间
 	 */
 	public void setString(String key, String data, Long timeout) {
-		stringRedisTemplate.opsForValue().set(key, data);
-		if (timeout != null) {
-			stringRedisTemplate.expire(key, timeout, TimeUnit.SECONDS);
+		try {
+
+			stringRedisTemplate.opsForValue().set(key, data);
+			if (timeout != null) {
+				stringRedisTemplate.expire(key, timeout, TimeUnit.SECONDS);
+			}
+
+		} catch (Exception e) {
+
 		}
+
+	}
+
+	/**
+	 * 开启Redis 事务
+	 * 
+	 *
+	 */
+	public void begin() {
+		// 开启Redis 事务权限
+		stringRedisTemplate.setEnableTransactionSupport(true);
+		// 开启事务
+		stringRedisTemplate.multi();
+
+	}
+
+	/**
+	 * 提交事务
+	 * 
+	 *
+	 */
+	public void exec() {
+		// 成功提交事务
+		stringRedisTemplate.exec();
+	}
+
+	/**
+	 * 回滚Redis 事务
+	 */
+	public void discard() {
+		stringRedisTemplate.discard();
 	}
 
 	/**
@@ -56,7 +90,7 @@ public class RedisUtil {
 
 	/**
 	 * 根据对应的key删除key
-	 *
+	 * 
 	 * @param key
 	 */
 	public Boolean delKey(String key) {
--- t-tian-service-api-impl/t-tian-service-api-impl-member/src/main/java/com/luyitian/son/member/service/impl/MemberLoginServiceImpl.java
+++ t-tian-service-api-impl/t-tian-service-api-impl-member/src/main/java/com/luyitian/son/member/service/impl/MemberLoginServiceImpl.java
@@ -12,9 +12,11 @@ import com.luyitian.son.member.mapper.entity.UserTokenDo;
 import com.luyitian.son.member.service.MemberLoginService;
 import com.luyitian.son.utils.MD5Util;
 import com.luyitian.son.utils.tonken.GenerateToken;
+import com.luyitian.son.utils.transaction.RedisDataSoureceTransaction;
 import org.apache.commons.lang.StringUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,78 +28,97 @@ public class  MemberLoginServiceImpl extends BaseApiService<JSONObject> implemen
     private GenerateToken generateToken;
    @Autowired
     private UserTokenMapper userTokenMapper;
+   @Autowired
+    private RedisDataSoureceTransaction redisDataSoureceTransaction;
     @Override
     public synchronized BaseResponse<JSONObject>  login(@RequestBody  UserLogInputDTO userLoginInpDTO) {
-       //1.验证参数
+        //1.验证参数
         String mobile = userLoginInpDTO.getMobile();
-        if(StringUtils.isEmpty(mobile))
-        {
-            return  setResultError("手机号码不能为空");
+        if (StringUtils.isEmpty(mobile)) {
+            return setResultError("手机号码不能为空");
         }
         String password = userLoginInpDTO.getPassword();
-        if(StringUtils.isEmpty(password))
-        {
+        if (StringUtils.isEmpty(password)) {
             return setResultError("密码不能为空");
         }
         //2. 对登录密码进行加密,需要加密后对比
-        String newPassword= MD5Util.MD5(password);
+        String newPassword = MD5Util.MD5(password);
         String loginType = userLoginInpDTO.getLoginType();
-        String deviceInfor=userLoginInpDTO.getDeviceInfor();
-        if(StringUtils.isEmpty(deviceInfor))
-        {
+        String deviceInfor = userLoginInpDTO.getDeviceInfor();
+        if (StringUtils.isEmpty(deviceInfor)) {
             return setResultError("登陆设备不能为空");
         }
-        if(StringUtils.isEmpty(loginType))
-        {
+        if (StringUtils.isEmpty(loginType)) {
             return setResultError("登录类型不能为空");
         }
         /**
          * 只允许从PC,ANDROID,IOS上登录，并且通一个时候只允许一个设备登录（唯一登录）
          * 目的是现在登录范围
          */
-        if(!(loginType.equalsIgnoreCase(Constants.MEMBER_LOGIN_TYPE_ANDROID
-                 ) || loginType.equalsIgnoreCase(Constants.MEMBER_LOGIN_TYPE_IOS)
-                || loginType.equalsIgnoreCase(Constants.MEMBER_LOGIN_TYPE_PC)))
-        {
+        if (!(loginType.equalsIgnoreCase(Constants.MEMBER_LOGIN_TYPE_ANDROID
+        ) || loginType.equalsIgnoreCase(Constants.MEMBER_LOGIN_TYPE_IOS)
+                || loginType.equalsIgnoreCase(Constants.MEMBER_LOGIN_TYPE_PC))) {
             return setResultError("登录环境存在问题，请确认你的登录设备");
         }
         //3.手机号码加密码进行登录,判断用户是否存在
-        UserDO userDO = userMapper.login(mobile,newPassword);
-        if(null==userDO)
-        {
+        UserDO userDO = userMapper.login(mobile, newPassword);
+        if (null == userDO) {
             return setResultError("用户名称或密码错误");
         }
         //用户登录TONKEN,session,TONKEN相当于SESSION的ID，
         // 用户每一个端登录成功都会生成一个令牌，是零时的，而且唯一的，存在REDIS中作为REDISkey
-        //4.生成TONKEN
-        Long userId = userDO.getUserId();
-         //5.根据USERID+logintype+isavalie查询当前登录类型的登陆过没有
-        UserTokenDo userTokenDo = userTokenMapper.selectByUserIdAndLoginType(userId, loginType);
-       if(null!=userTokenDo)
-       {
-           String token = userTokenDo.getToken();
-           //从缓存中删除TONKEN
-           Boolean isremoveTonken = generateToken.removeToken(token);
-           if(isremoveTonken)
-           {
-               //将TONKEN的状态改为1
-               userTokenMapper.updateTokenAvailability(token);
-           }
-       }
+        //4.生成TONKEN     dis中作为rediskey value userid
+        TransactionStatus transactionStatus = null;
+        try {
+            Long userId = userDO.getUserId();
+            //5.根据USERID+logintype+isavalie查询当前登录类型的登陆过没有
+            UserTokenDo userTokenDo = userTokenMapper.selectByUserIdAndLoginType(userId, loginType);
+            //开启REDIS和 数据库同步数据
+            transactionStatus = redisDataSoureceTransaction.begin();
+            if (null != userTokenDo) {
+                String token = userTokenDo.getToken();
+                //从缓存中删除TONKEN
+                Boolean isremoveTonken = generateToken.removeToken(token);
+                if (isremoveTonken) {
+                    //将TONKEN的状态改为1
+                    int updateToTokenAvailability = userTokenMapper.updateTokenAvailability(token);
+                    if (!toDaoResult(updateToTokenAvailability)) {
+                        return setResultError("系统错误");
+                    }
+                }
+            }
 
-        //6.获取令牌,令牌前缀+登录类型。保证在REDIS的值为USERid
-        String keyprefix=Constants.MEMBER_TOKEN_KEYPREFIX+loginType;
-        String  newTonken= generateToken.createToken(keyprefix, userId+"");
-        UserTokenDo userToken=new UserTokenDo();
-        userToken.setUserId(userId);
-        userToken.setLoginType(userLoginInpDTO.getLoginType());
-        userToken.setToken(newTonken);
-        userToken.setDeviceInfor(userLoginInpDTO.getDeviceInfor());
-        userTokenMapper.insertUserToken(userToken);
-        JSONObject data=new JSONObject();
-        data.put("userToken", newTonken);
-        //1.插入新的tonken
+            //6.获取令牌,令牌前缀+登录类型。保证在REDIS的值为USERid, 注意变量在什么时候用，就在什么时候声明
 
-        return setResultSuccess(data);
+            UserTokenDo userToken = new UserTokenDo();
+            userToken.setUserId(userId);
+            userToken.setLoginType(userLoginInpDTO.getLoginType());
+            String keyprefix = Constants.MEMBER_TOKEN_KEYPREFIX + loginType;
+            String newTonken = generateToken.createToken(keyprefix, userId + "");
+            userToken.setToken(newTonken);
+            userToken.setDeviceInfor(userLoginInpDTO.getDeviceInfor());
+            int insertUserToken = userTokenMapper.insertUserToken(userToken);
+            if (!toDaoResult(insertUserToken)) {
+                redisDataSoureceTransaction.rollback(transactionStatus);
+                return setResultError("系统错误!");
+            }
+            JSONObject data = new JSONObject();
+            data.put("userToken", newTonken);
+            redisDataSoureceTransaction.commit(transactionStatus);
+            return setResultSuccess(data);
+        } catch (Exception e) {
+            try
+            {
+                redisDataSoureceTransaction.rollback(transactionStatus);
+            }catch (Exception e1)
+            {
+
+            }
+            return setResultError("系统错误");
+
+        }
     }
-}
+
+    }
+
+
